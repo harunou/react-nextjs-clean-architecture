106e8579359346a04938423be3406f4697003fab
harunou <dev.harunou@gmail.com>
2025-04-11 21:04:14 +0200

    Page: remove form tag

--- app/page.tsx
+++ app/page.tsx
@@ -8,22 +8,18 @@ export default async function Counter() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-white p-6 rounded-lg shadow-md">
         <div className="text-3xl font-bold text-gray-800">Count: {count}</div>
         <div className="flex gap-4">
-          <form action={addButtonClicked}>
-            <button
-              className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 active:bg-blue-700 transition cursor-pointer"
-              type="submit"
-            >
-              Add
-            </button>
-          </form>
-          <form action={removeButtonClicked}>
-            <button
-              className="px-6 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 active:bg-red-700 transition cursor-pointer"
-              type="submit"
-            >
-              Remove
-            </button>
-          </form>
+          <button
+            className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 active:bg-blue-700 transition cursor-pointer"
+            onClick={addButtonClicked}
+          >
+            Add
+          </button>
+          <button
+            className="px-6 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 active:bg-red-700 transition cursor-pointer"
+            onClick={removeButtonClicked}
+          >
+            Remove
+          </button>
         </div>
       </main>
     </div>
