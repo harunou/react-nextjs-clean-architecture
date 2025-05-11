13c842ce29477fff398bd03032e9a659688f10af
harunou <dev.harunou@gmail.com>
2025-05-11 13:46:35 +0200

    Counter: add experimental controller

--- app/counter-client-components-1/Counter/Counter.tsx
+++ app/counter-client-components-1/Counter/Counter.tsx
@@ -1,24 +1,64 @@
 "use client";
 
-import { JSX, startTransition, useActionState } from "react";
+import {
+  JSX,
+  startTransition,
+  useActionState,
+  useEffect,
+  useState,
+} from "react";
 import { Count } from "./Count/Count";
-import { addButtonClicked, removeButtonClicked } from "./controller";
+import {
+  addButtonClicked,
+  componentMounted,
+  removeButtonClicked,
+} from "./controller";
 
 export function Counter(): JSX.Element | null {
-  const [, addButtonClickedAction, addButtonClickedPending] = useActionState(
-    addButtonClicked,
-    null
-  );
-  const [, removeButtonClickedAction, removeButtonClickedPending] =
-    useActionState(removeButtonClicked, null);
+  const [count, setCount] = useState(0);
+  const [
+    addButtonClickedState,
+    addButtonClickedAction,
+    addButtonClickedPending,
+  ] = useActionState(addButtonClicked, count);
+  const [
+    removeButtonClickedState,
+    removeButtonClickedAction,
+    removeButtonClickedPending,
+  ] = useActionState(removeButtonClicked, count);
+
+  useEffect(() => {
+    const initialize = async () => {
+      const count = await componentMounted();
+      setCount(count);
+    };
+    initialize();
+  }, [setCount]);
+
+  useEffect(() => {
+    setCount(addButtonClickedState);
+  }, [addButtonClickedState]);
+
+  useEffect(() => {
+    setCount(removeButtonClickedState);
+  }, [removeButtonClickedState]);
+
+  const a = async () => {
+    const d = await addButtonClicked();
+    setCount(d);
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gray-100">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-white p-6 rounded-lg shadow-md">
         <pre className="bg-gray-200 text-black font-mono p-2 rounded overflow-x-auto w-full max-w-md text-sm mb-2 min-w-[350px]">
           {JSON.stringify(
             {
+              addButtonClickedState,
               addButtonClickedPending,
+              removeButtonClickedState,
               removeButtonClickedPending,
+              count,
             },
             null,
             " "
@@ -26,6 +66,12 @@ export function Counter(): JSX.Element | null {
         </pre>
         <Count />
         <div className="flex gap-4">
+          <button
+            className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 active:bg-blue-700 transition cursor-pointer"
+            onClick={() => a()}
+          >
+            Add
+          </button>
           <button
             className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 active:bg-blue-700 transition cursor-pointer"
             onClick={() => startTransition(() => addButtonClickedAction())}
--- app/counter-client-components-1/Counter/controller.tsx
+++ app/counter-client-components-1/Counter/controller.tsx
@@ -1,18 +1,22 @@
 "use server";
 
+import { countSelector } from "@/selectors/countSelector";
 import { decrementCountUseCase } from "@/useCases/decrementCount";
 import { incrementCountUseCase } from "@/useCases/incrementCount";
 import { sleep } from "@/utils/sleep";
-import { revalidatePath } from "next/cache";
 
 export const addButtonClicked = async () => {
   await sleep(1000);
   await incrementCountUseCase(2);
-  revalidatePath("/counter-client-components-1");
+  return countSelector();
 };
 
 export const removeButtonClicked = async () => {
   await sleep(1000);
   await decrementCountUseCase();
-  revalidatePath("/counter-client-components-1");
+  return countSelector();
+};
+
+export const componentMounted = async () => {
+  return countSelector();
 };
